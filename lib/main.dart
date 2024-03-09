d36a0daeb7151d77bd114bc733935a476f3ad007
daniel.solomon <daniel.solomon@plpsd.net>
2024-03-09 15:31:41 -0600

    more work

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,83 @@
-import 'package:autoscale_tabbarview/autoscale_tabbarview.dart';
 import 'package:flutter/material.dart';
-void main()=>runApp(const MyApp());
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-    //showSemanticsDebugger: true,
-      home: HomePage()
-    );
+      home:  HomePage());
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
-  late TabController tabController;
-  int tabIndex=0;
-overflowContent(){
-  return Column(children: [
-    for(int i=0; i<100; i++)Text("Content $i")
-    ]);
-  }
-
-  @override
-  void initState() {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+ late TabController tabController;
+ List<int> childCount=[3,3,100];
+ @override
+ void initState() {
     // TODO: implement initState
     super.initState();
     tabController=TabController(length: 3, vsync: this);
-  tabController.addListener(() => setState(() {
-    
-  }));
+  }
+  scrollOrNot({required int numOfChildren}){
+    print("number of children is $numOfChildren in scrollornot");
+
+    if(numOfChildren<8){ return const NeverScrollableScrollPhysics();}
+    else{return const AlwaysScrollableScrollPhysics();}
   }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
-      body: 
-      CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            flexibleSpace: Container(color: Colors.blue,),
-            expandedHeight: 200,
-            backgroundColor: Colors.blue,
-            bottom:TabBar(
-         
-              controller: tabController,
-              indicatorColor: Colors.red,
-              tabs: const [Tab(text:"Tab 1"), Tab(text:"Tab 2"), Tab(text:"Tab 3")]),
-          ),
+    return  NestedScrollView(
+headerSliverBuilder:(context, innerBoxIsScrolled)=>[
+ SliverAppBar(
+  expandedHeight: 300,
+  floating: true,
+  bottom: TabBar(
+    onTap: (value){
+     setState(() {
+      tabController.index=value;       
+     });
+    },
+    controller: tabController,
+    indicatorColor: Colors.red,
+    tabs: const [Text("Tab 1"),Text("Tab 2"),Text("Tab 3")]),
+)
+],
+body: TabBarView(
+  controller: tabController,
+  children: [
+  TabContent(numOfChildren: childCount[0]),
+  TabContent(numOfChildren: childCount[1]),
+  TabContent(numOfChildren: childCount[2]),
+  
 
-
-
-
-   SliverFillRemaining(
-      child:TabBarView(
-        controller: tabController,
-        children:  [
-                  ListView.builder(
-          itemCount:2 ,
-          itemBuilder: (_, index)=>ListTile(title: Text("Content $index"),)),
-                  ListView.builder(
-          itemCount:3 ,
-          itemBuilder: (_, index)=>ListTile(title: Text("Content $index"),)),
-
-Wrap(children: [overflowContent()],)
-  /*ListView.builder(
-      physics: null,  
-          itemCount:50 ,
-          itemBuilder: (_, index)=>ListTile(title: Text("Content $index"),))
-    */
-        ]),
-    )
-       
-        ],
-      ),
-
-    
-    );
-
+]),
+);
   }
+}
+
+class TabContent extends StatelessWidget {
+  final int numOfChildren;
+  const TabContent({super.key, required this.numOfChildren});
+@override
+Widget build(BuildContext context){
+ print("in the class the num of children is $numOfChildren");
+  return Scaffold(body:
+  CustomScrollView(
+  physics: numOfChildren<5?const NeverScrollableScrollPhysics(): const AlwaysScrollableScrollPhysics(),
+    slivers:[
+   SliverList.builder(
+    itemCount: numOfChildren,
+    itemBuilder: (context, index)=> ListTile(title: Text("item $index"),))
+ 
+  ])
+  );
+
+}
 }
